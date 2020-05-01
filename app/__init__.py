9148de63c06fca7d0eb5024480303cbeb8c057d0
Eric Lam <elam00@stuy.edu>
2020-05-01 19:40:50 -0400

    connected database

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os, sys, csv
 from flask import Flask, render_template, jsonify
-from tables import db
+from tables import db, Countries, Cases, Deaths, Recovered
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
@@ -10,41 +10,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
 db.init_app(app)
 
 
-def get_data():
-    parse = {}
-    populations = {}
-    with open('./app/static/data/population.csv','r') as file:
-        reader = csv.reader(file)
-        next(reader)
-        for row in reader:
-            populations[row[1]] = int(row[2])
-    with open('./app/static/data/covid19_confirmed.csv', 'r') as file:
-        reader = csv.reader(file)
-        next(reader)
-        next(reader)
-        for row in reader:
-            date = row[4]
-            if date not in list(parse):
-                parse[date] = {}
-            if row[6] == '':
-                continue
-            name = rename[row[1]] if row[1] in list(rename) else row[1]
-            if row[1] == 'France':
-                pop = populations['FRA']
-            elif row[1] == 'Netherlands':
-                pop = populations['NLD']
-            elif row[1] == 'United Kingdom':
-                pop = populations['GBR']
-            elif row[1] == 'Canada':
-                pop = populations['CAN']
-            else:
-                pop = populations[row[6]]
-            if name in list(parse[date]):
-                parse[date][name][0] = int(parse[date][name][0]*pop+int(row[5]))/pop
-                parse[date][name][1] += int(row[5])
-            else:
-                parse[date][name] = [int(row[5])/pop, int(row[5])]
-    return parse
+def get_data(table):
+    data = {}
+    for entry in table.query.all():
+        date = entry.date
+        country = entry.country
+        if date not in data:
+            data[date] = {}
+        data[date][country.name] = [entry.amount/country.population, entry.amount]
+    return data
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -52,9 +26,19 @@ def root():
     return render_template('index.html')
 
 
-@app.route('/data')
-def infections():
-    return jsonify(get_data())
+@app.route('/data/cases')
+def cases():
+    return jsonify(get_data(Cases))
+
+
+@app.route('/data/deaths')
+def deaths():
+    return jsonify(get_data(Deaths))
+
+
+@app.route('/data/recoveries')
+def recoveries():
+    return jsonify(get_data(Recovered))
 
 
 if __name__ == '__main__':
