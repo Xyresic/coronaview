dbc65683ce053458892efaf535f008d3e407898f
Joseph Lee <jlee01@stuy.edu>
2020-05-13 14:27:16 -0400

    showing d3 econ data

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask, render_template, jsonify
-from tables import db, Countries, Cases, Deaths, Recovered
+from tables import db, Countries, Cases, Deaths, Recovered, Company, DailyData, Sector
+
+import urllib.request as urllib
+import json
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
@@ -10,60 +13,63 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
 # start database
 db.init_app(app)
 with app.app_context():
-    db.create_all()
-    url = 'https://pkgstore.datahub.io/core/s-and-p-500-companies-financials/constituents-financials_json/data/ddf1c04b0ad45e44f976c1f32774ed9a/constituents-financials_json.json?fbclid=IwAR03-dtqPvXAiITWjGLAuWN_qLK2aYd0Lhhk_Q5lMKh3cIUVW-KfgNwa4Fs'
-    url_company_1 = "https://financialmodelingprep.com/api/v3/historical-price-full/"
-    url_company_2 = "?serietype=line"
-    hdr = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-        'Accept-Encoding': 'none',
-        'Accept-Language': 'en-US,en;q=0.8',
-        'Connection': 'keep-alive'
-    }
-    req = urllib.Request(url, headers=hdr)
-    data = json.loads(urllib.urlopen(req).read())
-    i = 0
-    end = len(data)
-    set_of_sectors = set()
-    #handles S&P 500 Index data
-    S_P = Company('S&P 500','None','None',0)
-    url_SP = 'https://financialmodelingprep.com/api/v3/historical-price-full/index/%5EGSPC'
-    req_SP = urllib.Request(url_SP, headers=hdr)
-    data_SP = json.loads(urllib.urlopen(req_SP).read())['historical']
-    for j in range(0,365):
-        daily_data = DailyData(data_SP[j]['date'],data_SP[j]['close'])
-        S_P.data_points.append(daily_data)
-        db.session.add(daily_data)
-    #going through all the companies in S&P 500 while putting them in sectors and getting their daily data over a year
-    while (i < end):
-        sector = None
-        #create company
-        company = Company(data[i]['Name'],data[i]['Sector'],data[i]['Symbol'],data[i]['Market Cap'])
-        #create a sector or add the compnay to a sector
-        if data[i]['Sector'] not in set_of_sectors:
-            set_of_sectors.add(data[i]['Sector'])
-            sector = Sector(data[i]['Sector'])
-            sector.companies.append(company)
-        else:
-            sector = Sector.query.filter_by(name = data[i]['Sector']).first()
-            sector.companies.append(company)
-        #get historical data on that company
-        req_company = urllib.Request(url_company_1 + data[i]['Symbol'] + url_company_2, headers=hdr)
-        data_company = json.loads(urllib.urlopen(req_company).read())
-        if len(data_company) > 0:
-            print(data_company['symbol'])
-            data_company = data_company['historical'][len(data_company['historical'])-365:len(data_company['historical'])]
-            for j in range(0,len(data_company)):
-                daily_data = DailyData(data_company[j]['date'],data_company[j]['close'])
-                company.data_points.append(daily_data)
-                db.session.add(daily_data)
-        db.session.add(sector)
-        db.session.add(company)
-        i += 1
-    db.session.add(S_P)
-    db.session.commit()
+    sectors = Sector.query.all()
+    print(sectors)
+# with app.app_context():
+#     db.create_all()
+#     url = 'https://pkgstore.datahub.io/core/s-and-p-500-companies-financials/constituents-financials_json/data/ddf1c04b0ad45e44f976c1f32774ed9a/constituents-financials_json.json?fbclid=IwAR03-dtqPvXAiITWjGLAuWN_qLK2aYd0Lhhk_Q5lMKh3cIUVW-KfgNwa4Fs'
+#     url_company_1 = "https://financialmodelingprep.com/api/v3/historical-price-full/"
+#     url_company_2 = "?serietype=line"
+#     hdr = {
+#         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+#         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#         'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+#         'Accept-Encoding': 'none',
+#         'Accept-Language': 'en-US,en;q=0.8',
+#         'Connection': 'keep-alive'
+#     }
+#     req = urllib.Request(url, headers=hdr)
+#     data = json.loads(urllib.urlopen(req).read())
+#     i = 0
+#     end = len(data)
+#     set_of_sectors = set()
+#     #handles S&P 500 Index data
+#     S_P = Company('S&P 500','None','None',0)
+#     url_SP = 'https://financialmodelingprep.com/api/v3/historical-price-full/index/%5EGSPC'
+#     req_SP = urllib.Request(url_SP, headers=hdr)
+#     data_SP = json.loads(urllib.urlopen(req_SP).read())['historical']
+#     for j in range(0,365):
+#         daily_data = DailyData(data_SP[j]['date'],data_SP[j]['close'])
+#         S_P.data_points.append(daily_data)
+#         db.session.add(daily_data)
+#     #going through all the companies in S&P 500 while putting them in sectors and getting their daily data over a year
+#     while (i < end):
+#         sector = None
+#         #create company
+#         company = Company(data[i]['Name'],data[i]['Sector'],data[i]['Symbol'],data[i]['Market Cap'])
+#         #create a sector or add the compnay to a sector
+#         if data[i]['Sector'] not in set_of_sectors:
+#             set_of_sectors.add(data[i]['Sector'])
+#             sector = Sector(data[i]['Sector'])
+#             sector.companies.append(company)
+#         else:
+#             sector = Sector.query.filter_by(name = data[i]['Sector']).first()
+#             sector.companies.append(company)
+#         #get historical data on that company
+#         req_company = urllib.Request(url_company_1 + data[i]['Symbol'] + url_company_2, headers=hdr)
+#         data_company = json.loads(urllib.urlopen(req_company).read())
+#         if len(data_company) > 0:
+#             print(data_company['symbol'])
+#             data_company = data_company['historical'][len(data_company['historical'])-365:len(data_company['historical'])]
+#             for j in range(0,len(data_company)):
+#                 daily_data = DailyData(data_company[j]['date'],data_company[j]['close'])
+#                 company.data_points.append(daily_data)
+#                 db.session.add(daily_data)
+#         db.session.add(sector)
+#         db.session.add(company)
+#         i += 1
+#     db.session.add(S_P)
+#     db.session.commit()
 
 def get_data(table):
     data = {}
@@ -88,6 +94,10 @@ def get_data(table):
 @app.route('/', methods=['GET', 'POST'])
 def root():
     return render_template('index.html')
+
+@app.route('/econ', methods=['GET', 'POST'])
+def root():
+    return render_template('econ.html')
 
 
 @app.route('/data/<country>')
@@ -114,6 +124,9 @@ def deaths():
 def recoveries():
     return jsonify(get_data(Recovered))
 
+@app.route('/data/<sector>')
+def sectors():
+    return jsonify(get_data(Sector))
 
 if __name__ == '__main__':
     app.debug = True
