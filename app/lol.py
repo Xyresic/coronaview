bb367bc4cab33e5ef6a702739fbcf45573794a97
Joseph Lee <jlee01@stuy.edu>
2020-05-13 22:33:13 -0400

    fixes

--- app/lol.py
+++ app/lol.py
@@ -3,7 +3,7 @@ def main():
     a = datetime.datetime.today()
     numdays = 180
     dateList = []
-    for x in range (0, numdays,2):
+    for x in range (0, numdays):
         date = a - datetime.timedelta(days = x)
         dateList.append(date.strftime('%Y-%m-%d'))
 
--- app/static/js/econ.js
+++ app/static/js/econ.js
@@ -1,3 +1,6 @@
+var clear = document.getElementById("yo").innerHTML;
+var url = "/data/sector/"+clear.substring(1,clear.length);
+console.log(url);
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 30, left: 60},
     width = 460 - margin.left - margin.right,
@@ -14,22 +17,21 @@ var svg = d3.select("#my_dataviz")
 
 var parseTime = d3.timeParse("%Y-%m-%d");
 //Read the data
-d3.json("/data/sector/Industrials").then(d => {
+d3.json(url).then(d => {
       d = d['points']
       console.log(d)
       d.forEach(function(da) {
            da['date'] = parseTime(da['date']);
            da['price'] = +da['price'];
       });
-      console.log(d)
+      console.log(d3.extent(d, function(a) {return a['date']; }))
       // Add X axis --> it is a date format
       var x = d3.scaleTime()
-        .domain(d3.extent(d, function(d) { return d['date']; }))
+        .domain(d3.extent(d, function(a) { console.log(a['date']);return a['date']; }))
         .range([ 0, width ]);
       svg.append("g")
         .attr("transform",`${ "translate(0," + height + ")"}`)
         .call(d3.axisBottom(x));
-
       // Add Y axis
       var y = d3.scaleLinear()
         .domain([0, d3.max(d, function(d) { return +d['price']; })])
@@ -55,7 +57,7 @@ d3.json("/data/sector/Industrials").then(d => {
 //     height = 500 - margin.top - margin.bottom;
 //
 // // parse the date / time
-// var parseTime = d3.timeParse("%d-%b-%y");
+// var parseTime = d3.timeParse("%Y-%m-%d");
 //
 // // set the ranges
 // var x = d3.scaleTime().range([0, width]);
@@ -63,8 +65,8 @@ d3.json("/data/sector/Industrials").then(d => {
 //
 // // define the line
 // var valueline = d3.line()
-//     .x(function(d) { return x(d.date); })
-//     .y(function(d) { return y(d.price); });
+//     .x(function(d) { return x(d['date']); })
+//     .y(function(d) { return y(d['price']); });
 //
 // // append the svg obgect to the body of the page
 // // appends a 'group' element to 'svg'
@@ -80,14 +82,16 @@ d3.json("/data/sector/Industrials").then(d => {
 // d3.json("/data/sector/Industrials").then(function(data) {
 //
 //   // format the data
-//   data.forEach(function(d) {
-//       d.date = parseTime(d.date);
-//       d.price = +d.price;
+//   data = data['points']
+//   data.forEach(function(da) {
+//        da['date'] = parseTime(da['date']);
+//        da['price'] = +da['price'];
 //   });
+//   console.log(data)
 //
 //   // Scale the range of the data
-//   x.domain(d3.extent(data, function(d) { return d.date; }));
-//   y.domain([0, d3.max(data, function(d) { return d.price; })]);
+//   x.domain(d3.extent(data, function(d) { return d['date']; }));
+//   y.domain([0, d3.max(data, function(d) { return d['date']; })]);
 //
 //   // Add the valueline path.
 //   svg.append("path")
